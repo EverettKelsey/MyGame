450d2520358d1ec18c2cabd44fb0da20358aae4d
EverettKelsey <kelseyj@my.erau.edu>
2024-02-17 14:24:06 -0700

    added enemy movement

--- MyGame/Assets/Scripts/EnemyBehavior.cs
+++ MyGame/Assets/Scripts/EnemyBehavior.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public Transform patrolRoute;
+    public List<Transform> locations;
+
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+
     void onTriggerEnter(Collision other)
     {
         if (other.gameObject.name == "Player")
@@ -21,12 +28,32 @@ public class EnemyBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(agent.remainingDistance < 0.2f && agent != agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
 
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in  patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if (locations.Count == 0)
+            return;
+        agent.destination = locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % locations.Count;
     }
 }

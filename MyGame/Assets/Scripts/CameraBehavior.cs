c46a4652578314d6087ed211d343bc3605d48764
EverettKelsey <kelseyj@my.erau.edu>
2024-02-29 16:16:32 -0700

    minimap

--- MyGame/Assets/Scripts/CameraBehavior.cs
+++ MyGame/Assets/Scripts/CameraBehavior.cs
@@ -6,7 +6,6 @@ public class CameraBehavior : MonoBehaviour
 {
     public Vector3 camOffset = new Vector3(0f, 1.2f, -2.6f);
     private Transform target;
-    public Vector2 turn;
     // Start is called before the first frame update
     void Start()
     {

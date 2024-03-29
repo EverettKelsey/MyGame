735c8075524d86ee021cc22735f070a4e8abab5a
EverettKelsey <kelseyj@my.erau.edu>
2024-03-29 11:13:54 -0700

    added some of start screen

--- MyGame/Assets/Scripts/Control.cs
+++ MyGame/Assets/Scripts/Control.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Control : MonoBehaviour
 {
+    public GameObject Castle;
+    public GameObject rules;
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        rules.SetActive(false);
     }
     public void NextScene()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Rules()
+    {
+        Castle.SetActive(false);
+        rules.SetActive(true);
+    }
+
+    public void Home()
+    {
+        Castle.SetActive(true);
+        rules.SetActive(false);
     }
 }
